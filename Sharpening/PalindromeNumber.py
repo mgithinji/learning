f73372dee50cf18e0ceab33006510cd039f6c994
Morgan Githinji <githinjimorgan@gmail.com>
2022-06-21 17:22:33 -0700

    Create PalindromeNumber.py

---
+++ Sharpening/PalindromeNumber.py
@@ -0,0 +1,18 @@
+# leetcode: https://leetcode.com/problems/palindrome-number/
+
+# 1. create two lists of digits, one reversed and check equality.
+# 2. stringify the number, reverse the string, and check equality.
+
+def isPalindrome(x: int) -> bool:
+    a = str(x)
+    b = a[::-1]
+
+    if a == b:
+        return True
+    else:
+        return False
+
+x = 2322
+
+res = isPalindrome(x)
+print(res)
